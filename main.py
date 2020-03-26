7e601620ed28de503f82ae0e19ebd875c0f10b52
Matt D <mdonovan.nmkac@gmail.com>
2020-03-26 00:06:50 +0000

    You can now kill the monster with a weapon

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ class Player:
   my_plevel = int
   my_health = int
   my_attack = int
+  my_weapon = int
 
-  def __init__(self,name,role,health,attack):
+  def __init__(self,name,role,health,attack,weapon):
     self.my_name = name
     self.my_role = role
     self.my_plevel = 0
     self.my_health = health
     self.my_attack = attack
+    self.my_weapon = weapon
   def return_name(self):
     return self.my_name
   def return_role(self):
@@ -27,6 +29,8 @@ class Player:
     return self.my_health
   def return_attack(self):
     return self.my_attack
+  def return_weapon(self):
+    return self.my_weapon
   def attack(self):
     return randint(0,6) + self.my_attack
   def take_damage(self,dmg):
@@ -51,6 +55,8 @@ class Monster:
     return self.my_attack
   def return_dmg(self):
     return self.my_dmg
+  def take_damage(self,dmg):
+    self.my_health -= dmg
   
 class Player_role:
   def __init__(self,name,health,attack):
@@ -71,26 +77,35 @@ class Game:
   def return_level(self):
     return self.glevel
 
-role = [["cadet",5,3],["recruit",10,2]]
+role = [["cadet",5,3,0],["recruit",10,2,0]]
 monsterlist = [Monster("goblin",2,1,1),Monster("imp",3,2,1)]
+weaponlist = [[0,"knife",1],[1,"blunt short sword",2]]
 
 
 new_game = Game()
 new_game.setup()
 seed(1)
+mindex = 0
+rindex = 0
 
 print("Input your heroes name: ")
 player_name = input("name: ")
 
-nobody = Player(player_name,role[0][0],role[0][1],role[0][2])
+nobody = Player(player_name,role[rindex][0],role[rindex][1],role[rindex][2],role[rindex][3])
 
 while True:
+  pwpn = weaponlist[nobody.return_weapon()][1]
+  pdmg = weaponlist[nobody.return_weapon()][2]
+  
+  
+
   print(f"{nobody.return_name()} you are a {nobody.return_role()}")
   print(f"Current Health: {nobody.return_health()}")
   print(f"Current Attack: {nobody.return_attack()}")
+  print(f"Current Weapon: {pwpn}")
   print(f"Current Game Turn {new_game.return_turn()} you are on level {new_game.return_level()}")
 
-  print(f"You are facing a {monsterlist[0].return_name()}")
+  print(f"You are facing a {monsterlist[mindex].return_name()}")
 
   print("Type q to quit any other to continue")
   print("Type q to quit")
@@ -104,13 +119,17 @@ while True:
     clear()
     attackroll = nobody.attack()
     print(f"You perform an attack scoring a {attackroll}")
-    monsterattackroll=randint(0,6) + monsterlist[0].return_attack()
-    print(f"The {monsterlist[0].return_name()} gets an attack roll of {monsterattackroll}")
+    monsterattackroll=randint(0,6) + monsterlist[mindex].return_attack()
+    print(f"The {monsterlist[mindex].return_name()} gets an attack roll of {monsterattackroll}")
     if attackroll > monsterattackroll:
-      print(f"You have damanged the {monsterlist[0].return_name()}")
+      print(f"You have damanged the {monsterlist[mindex].return_name()}")
+      monsterlist[mindex].take_damage(pdmg)
+      if monsterlist[mindex].return_health() <= 0:
+        print(f"The {monsterlist[mindex].return_name()} has died")
+        mindex += 1
     else:
-      print(f"The {monsterlist[0].return_name()} has damaged you! You have taken {monsterlist[0].return_dmg()}")
-      nobody.take_damage(monsterlist[0].return_dmg())
+      print(f"The {monsterlist[mindex].return_name()} has damaged you! You have taken {monsterlist[mindex].return_dmg()}")
+      nobody.take_damage(monsterlist[mindex].return_dmg())
       print(f"You have {nobody.return_health()} health remaining")
       if nobody.return_health() <= 0:
         print(f"You have died")
